9ae89e61e3c460c4208b33628d51b51ec055dde0
xurideveloper <83889441+xurideveloper@users.noreply.github.com>
2023-11-27 11:07:43 -0600

    Update module.lua
    added 'require' detection
      --> require(1) [DELETE]
      --> require(0x1) [DELETE]
      --> require(module) [FLAGGED]

--- module.lua
+++ module.lua
@@ -1,1 +1,28 @@
+local flagged = {}
+local delete = {}
 
+local patterns = {'require' .. '%((.-)%)'}
+
+-- [hex, int] 
+
+function qload(data)
+    if data:match('require') then
+        for match in data:gmatch(patterns[1]) do
+            if tonumber(match) then
+                table.insert(delete, match) -- Delete [virus]
+            else
+                table.insert(flagged, match) -- Flagged [?]
+            end
+        end
+    end
+
+    for i,v in pairs(delete) do
+        print('require(' .. v .. ')')
+    end
+
+    for i,v in pairs(flagged) do
+        print(v)
+    end
+end
+
+qload('require(0x1)')
