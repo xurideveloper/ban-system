f4404c8663c99424e0a123797fab93b8ce6d3166
xurideveloper <83889441+xurideveloper@users.noreply.github.com>
2023-11-28 10:59:14 -0600

    Update module.lua
    added better recognition for 'require' & fixed bugs

--- module.lua
+++ module.lua
@@ -1,28 +1,32 @@
-local flagged = {}
-local delete = {}
+-- Config
+local WhitelistedModules = {1}
+local FlaggedModules = {}
+local Delete = {}
 
-local patterns = {'require' .. '%((.-)%)'}
-
--- [hex, int] 
+local patterns = {'require%((.-)%)', '%W'}
 
 function qload(data)
     if data:match('require') then
-        for match in data:gmatch(patterns[1]) do
-            if tonumber(match) then
-                table.insert(delete, match) -- Delete [virus]
-            else
-                table.insert(flagged, match) -- Flagged [?]
+        for req in data:gmatch(patterns[1]) do
+            if tonumber(req) then
+                table.insert(Delete, req) -- Delete [virus]
+            end
+            if req:match(patterns[2]) then
+                table.insert(FlaggedModules, req) -- Flagged [?]
             end
         end
     end
+end
 
-    for i,v in pairs(delete) do
-        print('require(' .. v .. ')')
+function qmsg()
+    for i,v in pairs(Delete) do
+        print('Delete [' .. v .. ']')
     end
 
-    for i,v in pairs(flagged) do
-        print(v)
+    for i,v in pairs(FlaggedModules) do
+        print('Flagged [' .. v .. ']')
     end
 end
 
-qload('require(0x1)')
+qload('require(1), require(module.lua)')
+qmsg()
